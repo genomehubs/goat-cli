49399c4388e7567636bb4e6f2a49de9eda29094b
Max Brown <mb39@sanger.ac.uk>
2022-04-12 12:49:12 +0100

    Fix docs URL.

--- make_docs.bash
+++ make_docs.bash
@@ -5,6 +5,6 @@ rm -rf ./docs
 # don't want dependencies
 cargo doc --no-deps --document-private-items
 # magic..?
-echo "<meta http-equiv=\"refresh\" content=\"0; url=goat-cli\">" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=goat_cli\">" > target/doc/index.html
 # copy to docs
 cp -r target/doc ./docs
