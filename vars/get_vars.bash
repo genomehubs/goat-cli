a7c7e0489b56043bc7ef8982cfe7b46c3dbcc383
Max Brown <max.brown@ed.ac.uk>
2022-09-17 11:33:42 +0100

    Update variables. Minor changes to get_vars.bash to get sed to run on Mac again.

--- vars/get_vars.bash
+++ vars/get_vars.bash
@@ -16,10 +16,10 @@ python3 vars_to_string.py taxon_vars.json > goat_taxon_variable_data.txt
 
 # replace the variable_data.rs
 # add two tabs to start of each line
-sed -i 's/^/\t\t/' goat_taxon_variable_data.txt
+sed -i -e 's/^/\t\t/' goat_taxon_variable_data.txt
 
 # remove in place the variables
-sed '/\/\/ automated input start taxon/,/\/\/ automated input end taxon/{//!d}' ../src/utils/variable_data.rs > ./temp.rs
+sed '/\/\/ automated input start taxon/,/\/\/ automated input end taxon/{//!d;}' ../src/utils/variable_data.rs > ./temp.rs
 
 # insert the new ones
 # get line first
@@ -40,9 +40,9 @@ curl -X 'GET' \
 
 python3 vars_to_string.py assembly_vars.json > goat_assembly_variable_data.txt
 
-sed -i 's/^/\t\t/' goat_assembly_variable_data.txt
+sed -i -e 's/^/\t\t/' goat_assembly_variable_data.txt
 
-sed '/\/\/ automated input start assembly/,/\/\/ automated input end assembly/{//!d}' ../src/utils/variable_data.rs > ./temp.rs
+sed '/\/\/ automated input start assembly/,/\/\/ automated input end assembly/{//!d;}' ../src/utils/variable_data.rs > ./temp.rs
 
 LINE=$(grep -n '\/\/ automated input start assembly' ./temp.rs | cut -d ":" -f 1)
 # make new file
@@ -52,3 +52,6 @@ LINE=$(grep -n '\/\/ automated input start assembly' ./temp.rs | cut -d ":" -f 1
 rm ../src/utils/variable_data.rs
 mv temp2.rs ../src/utils/variable_data.rs
 rm ./temp.rs
+
+# some sed strangeness here is generating extra files with -e as extensions?
+rm *-e
